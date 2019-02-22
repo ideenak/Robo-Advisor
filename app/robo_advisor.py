3aefb4f2d199f07ec035639ec9dd27066b78991c
ideenak <45948963+ideenak@users.noreply.github.com>
2019-02-22 17:50:42 -0500

    Update robo_advisor.py

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -2,9 +2,17 @@ from dotenv import load_dotenv
 import json
 import os
 import requests
-import pandas as pd 
+import pandas as pnd 
 from datetime import datetime
 
 load_dotenv() 
 
 apikey = os.environ.get("ALPHAVANTAGE_API_KEY")
+
+while True:
+    ticker = input("Please enter the ticker of your stock of choice: ") 
+    if ticker.isdigit:
+        print("Invalid entry: a stock ticker only uses characters - integers or symbols are not permitted")
+    else:
+        pull = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&apikey" + apikey)
+        
