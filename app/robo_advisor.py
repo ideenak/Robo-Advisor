42411f2a76b63facae8ab93732dfe7f6332cc4d3
ideenak <45948963+ideenak@users.noreply.github.com>
2019-02-22 15:21:56 -0500

    Add files via upload

---
+++ app/robo_advisor.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+import json
+import os
+import requests
+
+load_dotenv() # loads environment variables set in a ".env" file, including the value of the ALPHAVANTAGE_API_KEY variable
+
+# see: https://www.alphavantage.co/support/#api-key
+api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+#print("API KEY: " + api_key)
+
+symbol = "NFLX" # TODO: capture user input, like... input("Please specify a stock symbol: ")
+
+# see: https://www.alphavantage.co/documentation/#daily (or a different endpoint, as desired)
+# TODO: assemble the request url to get daily data for the given stock symbol...
+
+# TODO: use the "requests" package to issue a "GET" request to the specified url, and store the JSON response in a variable...
+
+# TODO: further parse the JSON response...
+
+# TODO: traverse the nested response data structure to find the latest closing price and other values of interest...
+latest_price_usd = "$100,000.00"
+
+#
+# INFO OUTPUTS
+#
+
+# TODO: write response data to a CSV file
+
+# TODO: further revise the example outputs below to reflect real information
+print("-----------------")
+print(f"STOCK SYMBOL: {symbol}")
+print("RUN AT: 11:52pm on June 5th, 2018")
+print("-----------------")
+print("LATEST DAY OF AVAILABLE DATA: June 4th, 2018")
+print(f"LATEST DAILY CLOSING PRICE: {latest_price_usd}")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-----------------")
+print("RECOMMENDATION: Buy!")
+print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
+print("-----------------")
