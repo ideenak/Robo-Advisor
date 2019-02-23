f8b15a5641b3f1d6d1cc8563000ddb864fb9b24d
ideenak <45948963+ideenak@users.noreply.github.com>
2019-02-22 19:25:54 -0500

    Update robo_advisor.py

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,15 +4,40 @@ import os
 import requests
 import pandas as pnd 
 from datetime import datetime
+import datetime
 
 load_dotenv() 
 
-apikey = os.environ.get("ALPHAVANTAGE_API_KEY")
+apikey = str(os.environ.get("ALPHAVANTAGE_API_KEY"))
+#Shoutout to @Chenmi1997 for the help here on the "str(...)" part"
+print("--------------------------------------------------- ")
+print(" ")
+print("WELCOME TO THE BLUECHIP STOCK PICKER ")
+print(" ")
+print("This application helps volatility traders choose stocks that have low volatility")
+print("in order to assist in decisionmaking with regards to determining which equities") 
+print("to write calls and puts for.")
+print(" ")
+
+print("Disclaimer: this is not professional investment advice")
+print(" ")
+
+print("--------------------------------------------------- ")
+
+print(" ")
+
 
 while True:
     ticker = input("Please enter the ticker of your stock of choice: ") 
-    if ticker.isdigit:
+    #Shoutout to @hiepnguyen for helping me realize a while loop is optimal for this
+    
+    if ticker.isdigit():
         print("Invalid entry: a stock ticker only uses characters - integers or symbols are not permitted")
     else:
-        pull = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&apikey" + apikey)
+        pull = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + ticker + "&apikey=" + apikey)
         
+        if "Error" in pull.text:
+            print("Error: stock either cannot be found or is not listed on Alpha Vantage - please enter another stock ticker")
+        else:
+            break  
+
